d5e6ae74db9237c5134317422930cd6cd355875a
Shubhashish Das <Shubhashish_Das@mckinsey.com>
2017-09-27 00:40:48 +0530

    Initial setup

---
+++ src/main/java/com/travelsmart/org/TravelSmartApplication.java
@@ -0,0 +1,12 @@
+package com.travelsmart.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelSmartApplication.class, args);
+	}
+}
